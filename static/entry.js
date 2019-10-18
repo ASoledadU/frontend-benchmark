a91cb13a1c6114998f3a5a413db179d0679432e8
Student <student@donation.local>
2019-10-18 15:16:27 -0500

    Attempted to add the feature of a like button. Organized the 3 HTML pages into their respective functions instead of being all the same thing.

--- static/entry.js
+++ static/entry.js
@@ -1,14 +1,29 @@
 const likes = document.querySelectorAll("#divOfLikes")
 
 for (const like of likes) {
-    const actualLike = like.querySelector("#like");
+    const actualLike = like.querySelector("#clickme");
     actualLike.addEventListener('click', () => {
         const url = "/entries/entry.likes/like"
         fetch(url)
-            .then(rsp => rsp.json())
+            .then(response => response.json())
             .then(data => {
                 const entryLikes = data.querySelector("#entryLikes")
                 entryLikes.textContent = Number(entryLikes.textContent) + 1;
             })
     })
 }
+
+function clickCounter() {
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.querySelector("#result").innerHTML = "Likes " + localStorage.clickcount;
+    }
+}
+
+clickCounter();
+
+
